0b96bf74b4a77f584357d1e8652e7954cdb90f31
Miraj Patel <mirajpatel@Mirajs-MacBook-Air.local>
2025-02-06 13:12:29 -0600

    complete dashboard, fix packages

--- src/app/dashboard/CardPurchaseSummary.tsx
+++ src/app/dashboard/CardPurchaseSummary.tsx
@@ -2,6 +2,14 @@ import { useGetDashboardMetricsQuery } from "@/state/api";
 import { TrendingDown, TrendingUp } from "lucide-react";
 import numeral from "numeral";
 import React from "react";
+import {
+  Area,
+  AreaChart,
+  ResponsiveContainer,
+  Tooltip,
+  XAxis,
+  YAxis,
+} from "recharts";
 
 const CardPurchaseSummary = () => {
   const { data, isLoading } = useGetDashboardMetricsQuery();
@@ -20,6 +28,7 @@ const CardPurchaseSummary = () => {
             <h2 className="text-lg font-semibold mb-2 px-7 pt-5">
               Purchase Summary
             </h2>
+            <hr />
           </div>
 
           <div>
@@ -29,11 +38,11 @@ const CardPurchaseSummary = () => {
                 <p className="text-2xl font-bold">
                   {lastDataPoint
                     ? numeral(lastDataPoint.totalPurchased).format("$0.00a")
-                    : 0}
+                    : "0"}
                 </p>
                 {lastDataPoint && (
                   <p
-                    className={`text-xs ${
+                    className={`text-sm ${
                       lastDataPoint.changePercentage! >= 0
                         ? "text-green-500"
                         : "text-red-500"
@@ -49,6 +58,35 @@ const CardPurchaseSummary = () => {
                 )}
               </div>
             </div>
+            <ResponsiveContainer width="100%" height={195} className="p-2">
+              <AreaChart
+                data={purchaseData}
+                margin={{ top: 0, right: 0, left: -50, bottom: 45 }}
+              >
+                <XAxis dataKey="date" tick={false} axisLine={false} />
+                <YAxis tickLine={false} tick={false} axisLine={false} />
+                <Tooltip
+                  formatter={(value: number) => [
+                    `$${value.toLocaleString("en")}`,
+                  ]}
+                  labelFormatter={(label) => {
+                    const date = new Date(label);
+                    return date.toLocaleDateString("en-US", {
+                      year: "numeric",
+                      month: "long",
+                      day: "numeric",
+                    });
+                  }}
+                />
+                <Area
+                  type="linear"
+                  dataKey="totalPurchased"
+                  stroke="#8884d8"
+                  fill="#8884d8"
+                  dot={true}
+                />
+              </AreaChart>
+            </ResponsiveContainer>
           </div>
         </>
       )}
